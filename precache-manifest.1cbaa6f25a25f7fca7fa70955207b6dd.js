74b789e0684d7ba61060748abd02c8a6f4600804
Kevin-Dewinter <kevin.dewinter@myglamm.com>
2020-06-29 20:06:23 +0530

    Updates

---
+++ precache-manifest.1cbaa6f25a25f7fca7fa70955207b6dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b492bb0c12b28b1373bd0e85f5e50fbb",
+    "url": "/open-weather/index.html"
+  },
+  {
+    "revision": "9c0fae630d9da8311d23",
+    "url": "/open-weather/static/css/main.fdb22a90.chunk.css"
+  },
+  {
+    "revision": "f6fe161f27aa1fe598bc",
+    "url": "/open-weather/static/js/2.7034cf61.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/open-weather/static/js/2.7034cf61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c0fae630d9da8311d23",
+    "url": "/open-weather/static/js/main.c78aac94.chunk.js"
+  },
+  {
+    "revision": "d8240cad61a6b40bd64e",
+    "url": "/open-weather/static/js/runtime-main.f6c7a3f9.js"
+  },
+  {
+    "revision": "a1b3557a2224e625f95ce57f00c669fb",
+    "url": "/open-weather/static/media/cloudy.a1b3557a.png"
+  },
+  {
+    "revision": "9e0e3133dab653f41cb480d1843b5f51",
+    "url": "/open-weather/static/media/sunny.9e0e3133.png"
+  }
+]);
